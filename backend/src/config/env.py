82884cef9d1f70ee98c0d91d09f317377c5f8f8f
Luciano Martín Gamberale <l.gamberale@mercapsoftware.com>
2025-05-10 15:21:17 -0300

    :bug: fix on backend

--- backend/src/config/env.py
+++ backend/src/config/env.py
@@ -4,8 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-if not all([os.getenv("DB_URL"), os.getenv("FRONTEND_URL")]):
+if not all([os.getenv("DB_URL")]):
     raise ValueError("Missing environment variables")
 
 DB_URL = str(os.getenv("DB_URL"))
-FRONTEND_URL = str(os.getenv("FRONTEND_URL"))
