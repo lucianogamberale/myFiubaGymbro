2f5dcac45c2644234debf474580782fd67c46cf2
Luciano Martín Gamberale <l.gamberale@mercapsoftware.com>
2025-05-11 19:25:14 +0000

    :construction: remove alembic

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from fastapi.concurrency import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine
 
 from .config.api_metadata import FASTAPI_METADATA
+from .config.env import DB_URL
+from .repositories.models.base_model import Base
 from .routers.user_foods import router as user_foods_router
 from .routers.users import router as users_router
 
-app = FastAPI(**FASTAPI_METADATA)
+engine = create_engine(DB_URL, echo=True)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Create tables on startup
+    Base.metadata.create_all(bind=engine)
+    yield  # Startup complete
+
+    # remove tables on shutdown
+    Base.metadata.drop_all(bind=engine)
+
+app = FastAPI(**FASTAPI_METADATA, lifespan=lifespan)
+
+
+
 
 app.add_middleware(
     CORSMiddleware,
--- backend/src/repositories/models/user_food.py
+++ backend/src/repositories/models/user_food.py
@@ -1,17 +1,22 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
-from src.repositories.models.user import User
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from .base_model import Base, Str
+from .base_model import Base, BigInt, Str
+from .user import User
 
 
 class UserFood(Base):
     __tablename__ = "user_foods"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[BigInt] = mapped_column(primary_key=True)
     food_name: Mapped[Str] = mapped_column()
     food_category: Mapped[Str] = mapped_column()
     calories: Mapped[float] = mapped_column()
     date_eaten: Mapped[str] = mapped_column()
-    user_id: Mapped[int] = mapped_column()
-    
+    user_id: Mapped[BigInt] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates="user_foods",
+    )
