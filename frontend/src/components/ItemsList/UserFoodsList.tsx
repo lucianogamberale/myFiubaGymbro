1fac63304bc6a45874b9b99bf2bcf60cb2c5fef6
Luciano Martín Gamberale <lgamberale@fi.uba.ar>
2025-05-25 04:40:42 +0000

    :construction: add to frontend user health data

--- frontend/src/components/ItemsList/UserFoodsList.tsx
+++ frontend/src/components/ItemsList/UserFoodsList.tsx
@@ -73,7 +73,7 @@ export const UserFoodsList = ({ updateUserFoods, onUpdateUserFoods }: Props) => 
                 ) :
                     <>
                         {userFoods.length === 0 && !loading ? (
-                            <div className="text-lg text-slate-400 text-center mt-3">Aún no tenés comidas cargadas.</div>
+                            <div className="text-lg text-slate-400 text-center mt-3">Aún no cargaste nada.</div>
                         ) : (
                             <div className="flex-grow overflow-auto">
                                 <div className="grid grid-rows-* gap-4 p-2 ml-2 mr-2">
